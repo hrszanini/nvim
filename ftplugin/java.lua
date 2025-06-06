9d64cedba243fc81a2c3da222ee41eeddd15fa37
Hamilton Zanini <hrszanini@gmail.com>
2025-06-06 05:49:49 -0300

    atualização

---
+++ ftplugin/java.lua
@@ -0,0 +1,66 @@
+local mason_registry = require("mason-registry")
+local installation_path_lsp = mason_registry.get_package("jdtls"):get_install_path()
+local installation_path_dap = mason_registry.get_package("java-debug-adapter"):get_install_path()
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = vim.fn.stdpath 'data' .. '/site/java/workspace-root/' .. project_name
+os.execute('mkdir -p ' .. workspace_dir)
+
+local status, jdtls = pcall(require, 'jdtls')
+if not status then
+	return
+end
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
+local config = {
+	cmd = {
+		-- '/usr/lib/jvm/java-21-openjdk/bin/java',
+		'$JAVA_HOME',
+		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+		'-Dosgi.bundles.defaultStartLevel=4',
+		'-Declipse.product=org.eclipse.jdt.ls.core.product',
+		'-Dlog.protocol=true',
+		'-Dlog.level=ALL',
+		'-Xmx1g',
+		'--add-modules=ALL-SYSTEM',
+		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+		'-javaagent:' .. installation_path_lsp .. '/lombok.jar',
+		'-jar', vim.fn.glob(installation_path_lsp .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+		'-configuration', installation_path_lsp .. '/config_linux',
+		'-data', workspace_dir
+	},
+	settings = {
+		java = {
+			signatureHelp = { enabled = true },
+			extendedClientCapabilities = extendedClientCapabilities,
+			maven = {
+				downloadSources = true,
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			references = {
+				includeDecompiledSources = true,
+			},
+			inlayHints = {
+				parameterNames = {
+					enabled = 'all', -- literals, all, none
+				},
+			},
+			format = {
+				enabled = false,
+			},
+		},
+	},
+
+	init_options = {
+		bundles = {
+			vim.fn.glob(
+				installation_path_dap .. "/extension/server/com.microsoft.java.debug.plugin-*.jar",
+				true)
+			}
+		},
+	}
+
+	require('jdtls').start_or_attach(config)
--- lua/plugins/code/mason.lua
+++ lua/plugins/code/mason.lua
@@ -2,100 +2,47 @@ return {
 	-- Main LSP Configuration
 	'neovim/nvim-lspconfig',
 	dependencies = {
-		-- Automatically install LSPs and related tools to stdpath for Neovim
-		-- Mason must be loaded before its dependents so we need to set it up here.
-		-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
 		{ 'mason-org/mason.nvim', opts = {} },
+		{ 'j-hui/fidget.nvim', opts = {} },
 		'mason-org/mason-lspconfig.nvim',
 		'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-		-- Useful status updates for LSP.
-		{ 'j-hui/fidget.nvim', opts = {} },
-
-		-- Allows extra capabilities provided by blink.cmp
 		'saghen/blink.cmp',
 	},
 	config = function()
-		-- Brief aside: **What is LSP?**
-		--
-		-- LSP is an initialism you've probably heard, but might not understand what it is.
-		--
-		-- LSP stands for Language Server Protocol. It's a protocol that helps editors
-		-- and language tooling communicate in a standardized fashion.
-		--
-		-- In general, you have a "server" which is some tool built to understand a particular
-		-- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-		-- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-		-- processes that communicate with some "client" - in this case, Neovim!
-		--
-		-- LSP provides Neovim with features like:
-		--  - Go to definition
-		--  - Find references
-		--  - Autocompletion
-		--  - Symbol Search
-		--  - and more!
-		--
-		-- Thus, Language Servers are external tools that must be installed separately from
-		-- Neovim. This is where `mason` and related plugins come into play.
-		--
-		-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-		-- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-		--  This function gets run when an LSP attaches to a particular buffer.
-		--    That is to say, every time a new file is opened that is associated with
-		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-		--    function will be executed to configure the current buffer
 		vim.api.nvim_create_autocmd('LspAttach', {
 			group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
 			callback = function(event)
-				-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-				-- to define small helper and utility functions so you don't have to repeat yourself.
-				--
-				-- In this case, we create a function that lets us more easily define mappings specific
-				-- for LSP related items. It sets the mode, buffer and description for us each time.
 				local map = function(keys, func, desc, mode)
 					mode = mode or 'n'
 					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
 				end
 
-				-- The following two autocommands are used to highlight references of the
-				-- word under your cursor when your cursor rests there for a little while.
-				--    See `:help CursorHold` for information about when this is executed
-				--
-				-- When you move your cursor, the highlights will be cleared (the second autocommand).
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-					local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-					vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.document_highlight,
-					})
 
-					vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.clear_references,
-					})
+				local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+				vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+					buffer = event.buf,
+					group = highlight_augroup,
+					callback = vim.lsp.buf.document_highlight,
+				})
 
-					vim.api.nvim_create_autocmd('LspDetach', {
-						group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-						callback = function(event2)
-							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-						end,
-					})
-				end
+				vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+					buffer = event.buf,
+					group = highlight_augroup,
+					callback = vim.lsp.buf.clear_references,
+				})
 
-				-- The following code creates a keymap to toggle inlay hints in your
-				-- code, if the language server you are using supports them
-				--
-				-- This may be unwanted, since they displace some of your code
-				if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-					map('<leader>th', function()
-						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-					end, '[T]oggle Inlay [H]ints')
-				end
+				vim.api.nvim_create_autocmd('LspDetach', {
+					group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+					callback = function(event2)
+						vim.lsp.buf.clear_references()
+						vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+					end,
+				})
+
+				map('<leader>th', function()
+					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+				end, '[T]oggle Inlay [H]ints')
 			end,
 		})
 
@@ -132,18 +79,10 @@ return {
 
 		local servers = {
 			clangd = {},
-			-- gopls = {},
+			gopls = {},
 			pylsp = {},
 			rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
-			-- ts_ls = {},
-			--
-
+			ts_ls = {},
 			lua_ls = {
 				-- cmd = { ... },
 				-- filetypes = { ... },
